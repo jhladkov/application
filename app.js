da339a1eaaafbb48941005bc243bc17d96357748
Zhenua <zhenua.gladkovv@gmail.com>
2021-06-06 16:03:55 +0300

    commit update

--- app.js
+++ app.js
@@ -2,8 +2,56 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.task');
 const taskTodoList = document.querySelector('.mask-list');
 const taskListDone = document.querySelector('.complete-list');
-// const selectTime = document.querySelector('.select')
-// const buttonSaveTime = document.querySelector('.save-time');
+
+
+// logic colors
+const header = document.querySelector('.header')
+const callColorChart = document.querySelector('.call-color-chart')
+const backgroundShadow = document.querySelector('.shadow')
+const colorChart = document.querySelector('.color-chart')
+const colorChartItemHeader = document.querySelectorAll('.color-chart-item-header')
+const colorChartItemButtons = document.querySelectorAll('.color-chart-item-button')
+let buttons = document.querySelectorAll('.our')
+
+const addColorManagement = () => {
+    backgroundShadow.classList.add('active');
+    colorChart.classList.add('active')
+}
+
+const removeColorManagement = () => {
+    backgroundShadow.classList.remove('active')
+    colorChart.classList.remove('active')
+}
+
+const buttonsAndHeaderStyle = (buttons, header) => {
+    if (buttons) {
+        buttons.forEach(item => {
+            item.style.backgroundColor = localStorage.getItem('colorStyleButtons')
+        })
+    }
+    if (header) {
+        header.style.backgroundColor = localStorage.getItem('colorStyleHeader')
+    }
+}
+colorChartItemHeader.forEach(item => {
+    item.onclick = async () => {
+        await localStorage.setItem('colorStyleHeader',item.id)
+        removeColorManagement()
+        buttonsAndHeaderStyle(null,header)
+    }
+})
+colorChartItemButtons.forEach(item => {
+    item.onclick = async () => {
+        await localStorage.setItem('colorStyleButtons',item.id)
+        removeColorManagement()
+        buttonsAndHeaderStyle(buttons)
+    }
+})
+
+callColorChart.onclick = () => addColorManagement()
+
+backgroundShadow.onclick = () => removeColorManagement()
+//
 
 let todoList = [];
 
@@ -26,7 +74,7 @@ const getTemplate = (value, id, status) => {
                 <span></span>
                 <p class="text">${value}</p>
               </div>
-              <button onclick="removeTask(${id})" class="btn-remove close">Remove</button>
+              <button onclick="removeTask(${id})" class="btn-remove close our">Remove</button>
             </li>`;
 }
 
@@ -41,12 +89,15 @@ const updateDOM = () => {
 
 const addListToHtml = (nodeList, status) => {
     nodeList.innerHTML = todoList.filter((item) => item.status === status).map((item) => item.text).join('');
+    buttons = document.querySelectorAll('.our')
+    buttonsAndHeaderStyle(buttons,header)
 }
 
 const removeTask = (id) => {
     todoList = todoList.filter((item) => item.id !== id); // отфильтруй массив что бы у его елементов не совпадал id с аргументом id
     updateDOM();
     setItemInLocalStorage(todoList);
+    buttonsAndHeaderStyle(buttons,header)
 }
 
 const reverseStatus = (item) => {
@@ -68,55 +119,23 @@ const changeTaskStatus = (id) => {
     })
     updateDOM();
     setItemInLocalStorage(todoList)
+    buttonsAndHeaderStyle(buttons,header)
 }
 
 const setItemInLocalStorage = (arr) => {
     localStorage.setItem('task', JSON.stringify(arr));
 }
-
-// buttonSaveTime.addEventListener('click', () => {
-//     if (selectTime.value === 'disable') return;
-//     notifyMe(parseInt(((selectTime.value * 60) * 60) * 1000));
-// })
-
-// const showNotification = () => {
-//     const notification = new Notification('To-Do List', {
-//         body: 'Пора выполнять задачи',
-//         icon: 'https://www.javascripttutorial.net/wp-content/uploads/2020/09/js.png'
-//     });
-//     notification.addEventListener('click', () => {
-//         window.open('https://jhladkov.github.io/application/', '_blank');
-//     });
-// }
-//test
-// const test = () => {
-//     let data = new Date();
-//     if (data.getMinutes() === 40) {
-//         clearInterval(intervalNotification)
-//         return showNotification()
-//     }
-// }
-//
-// const notifyMe = async () => {
-//     let granted = false;
-//
-//     if (Notification.permission === 'granted') {
-//         granted = true;
-//     } else if (Notification.permission !== 'denied') {
-//         let permission =  await Notification.requestPermission();
-//         granted = permission === 'granted' ? true : false;
-//     }
-//
-//     granted ? intervalNotification : console.log('Вы отменили сообщения');
-// }
-// let intervalNotification = setInterval(test, 1000)
-//
-
-window.onload = async () => {
+window.onload = () => {
     if (JSON.parse(localStorage.getItem('task'))) {
         todoList = JSON.parse(localStorage.getItem('task'));
         addListToHtml(taskTodoList, enums.taskStatuses.TODO);
         changeTaskStatus();
+    }
+    if (localStorage.getItem('colorStyleHeader')) {
+        buttonsAndHeaderStyle(null,header)
+    }
+    if (localStorage.getItem('colorStyleButtons')) {
+        buttonsAndHeaderStyle(buttons)
     }
 }
 
